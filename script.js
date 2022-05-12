2aac28a48d01a9c04fa579900420b0a05d33c289
Brigham Andersen <brighamband@gmail.com>
2022-05-12 14:57:38 -0600

    Got start, stop, and pause working

--- script.js
+++ script.js
@@ -1,28 +1,52 @@
-const currentPgH1 = document.querySelector('.current-pg-text')
-const timeH1 = document.querySelector('.time')
+const currentPgH1 = document.querySelector('.current-pg-text');
+const timeH1 = document.querySelector('.time');
+const togglePauseBtn = document.querySelector('.toggle-pause-btn');
 
 let secondsElapsed = 0;
+let isPaused = true;
 
 function goToCountdown() {
-  document.title = "Countdown"
-  currentPgH1.textContent = "Countdown"
+  document.title = "Countdown";
+  currentPgH1.textContent = "Countdown";
+  resetStopwatch();
 }
 
 function goToStopwatch() {
-  document.title = "Stopwatch"
-  currentPgH1.textContent = "Stopwatch"
+  document.title = "Stopwatch";
+  currentPgH1.textContent = "Stopwatch";
+  resetStopwatch();
 }
 
-function startStopwatch() {
-  setInterval(() => {
-    secondsElapsed += 1
-    const timeStr = secondsNumToHmsStr(secondsElapsed);
-    timeH1.textContent = timeStr;
-    document.title = timeStr + ' (Stopwatch)';
-  }, 1000)
+// Ongoing process in background that will increment seconds when enabled
+setInterval(() => {
+  if (!isPaused) {
+    secondsElapsed += 1;
+    renderTimeDisplay();
+  }
+}, 1000)
+
+function togglePause() {
+  isPaused = !isPaused;
+
+  isPaused ? 
+    togglePauseBtn.textContent = "Resume" 
+    : togglePauseBtn.textContent = "Pause";
+}
+
+function resetStopwatch() {
+  isPaused = true;
+  togglePauseBtn.textContent = "Start";
+  secondsElapsed = 0;
+  renderTimeDisplay();
 }
 
 // Ex: Converts 5 (seconds) into '00:00:05'
 function secondsNumToHmsStr() {
   return new Date(secondsElapsed * 1000).toISOString().substring(11, 19);
+}
+
+function renderTimeDisplay() {
+  const timeStr = secondsNumToHmsStr(secondsElapsed);
+  timeH1.textContent = timeStr;
+  document.title = timeStr + ' (Stopwatch)';
 }
