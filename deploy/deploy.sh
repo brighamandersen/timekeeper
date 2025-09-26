3fbd2fa7885cd14def99a786035f03329ea8c6a8
Brigham Andersen <brighambandersen@gmail.com>
2025-09-25 21:56:10 -0600

    deploy

---
+++ deploy/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Deploying timekeeper"
+
+# frontend
+
+cd /home/brig/code/timekeeper/
+npm run build
+
+# nginx
+
+sudo ln -sf /home/brig/code/timekeeper/deploy/nginx.conf /etc/nginx/conf.d/timekeeper.conf
+
+sudo nginx -t
+sudo systemctl reload nginx
+
+echo "Deployment complete for timekeeper"
